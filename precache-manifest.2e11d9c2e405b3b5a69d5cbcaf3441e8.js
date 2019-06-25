33449cc006333837f9d682eafcf57c49575e0cf4
Henry Valbuena <dev.henryvm@gmail.com>
2019-06-25 13:27:21 -0400

    Updates

---
+++ precache-manifest.2e11d9c2e405b3b5a69d5cbcaf3441e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda29bf6597a122ec2d4e5c44adfef1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c86d235cc4bdccb06098",
+    "url": "/static/css/main.57b7c038.chunk.css"
+  },
+  {
+    "revision": "011012c334bae0ac8b0f",
+    "url": "/static/js/2.4a6eaf1c.chunk.js"
+  },
+  {
+    "revision": "c86d235cc4bdccb06098",
+    "url": "/static/js/main.cd114c7f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
