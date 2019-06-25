9012fa282f7d24d87c779121015cda76e8dd4bca
Henry Valbuena <dev.henryvm@gmail.com>
2019-06-25 14:12:42 -0400

    Updates

---
+++ precache-manifest.343afb78d4c970d3808b0fb84953902b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb320c560e60168c617a5f6f0bbc450",
+    "url": "/henryvalbuena.react/index.html"
+  },
+  {
+    "revision": "33fe6c665da1270c586d",
+    "url": "/henryvalbuena.react/static/css/main.57b7c038.chunk.css"
+  },
+  {
+    "revision": "011012c334bae0ac8b0f",
+    "url": "/henryvalbuena.react/static/js/2.4a6eaf1c.chunk.js"
+  },
+  {
+    "revision": "33fe6c665da1270c586d",
+    "url": "/henryvalbuena.react/static/js/main.6722b7fc.chunk.js"
+  },
+  {
+    "revision": "9c7676116651530ac424",
+    "url": "/henryvalbuena.react/static/js/runtime~main.0371843b.js"
+  }
+]);
