995e8be59a2d30e62e0a9c6f2220b02dd8f15693
Henry Valbuena <dev.henryvm@gmail.com>
2020-07-30 18:09:19 -0400

    Added blue

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,60 +1,9 @@
-import React, { Component } from 'react';
-import TextContainer from '../components/TextContainer';
-import MenuBar from '../components/MenuBar';
-import ProjectCardList from '../components/ProjectCardList';
-import { Grid, Image } from 'semantic-ui-react';
-import projects from '../projectList';
-import SideMenu from '../components/SideMenu';
-import aboutMe from '../about';
-import ListContainer from '../components/ListContainer';
+import React from "react";
 
-class App extends Component {
-    state = { visible: false }
-    
-    showSideMenu = () => {
-        this.setState(prevState => ({
-            visible: !prevState.visible
-        }))
-    }
-    
-    render() {
-        return (
-            <div>
-                <MenuBar showSideMenu={this.showSideMenu}/>
-                <SideMenu visible={this.state.visible}>
-                    <Grid centered columns='equal' >
-                        <Grid.Row>
-                            <h1 className='header'>A little about me</h1>
-                        </Grid.Row>
-                        <Grid.Row stretched={false} verticalAlign="middle" centered>
-                            <Grid.Column verticalAlign='middle'>
-                                <Image src='https://res.cloudinary.com/devhenry/image/upload/c_scale,h_682/v1569000107/Jenova/img/me02.jpg' size='medium' rounded fluid={false} verticalAlign='middle' />
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <TextContainer text={aboutMe.intro}/>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <h2>Interests</h2>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <ListContainer list={aboutMe.interest}/>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <h2>Most recent projects</h2>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <ProjectCardList projects={projects}/>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <p>Developed by <b>Henry Valbuena</b> 2020</p>
-                        </Grid.Row>
-                        <Grid.Row></Grid.Row>
-                    </Grid>
-                </SideMenu>
-            </div>
-        );
-    }
+import Blue from "./Blue";
+
+function App(props) {
+  return <Blue />
 }
 
 export default App;
